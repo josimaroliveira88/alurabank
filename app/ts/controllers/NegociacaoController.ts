e1f5307d94ec6c8692966e7e0b496236df63741e
Josimar Alves de Oliveira <josimar.oliveira88@gmail.com>
2021-07-20 21:32:33 -0300

    aula 3 capítulo 5

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -9,7 +9,7 @@ class NegociacaoController {
         this.inputData = <HTMLInputElement> document.querySelector('#data');
         this.inputQuantidade = <HTMLInputElement> document.querySelector('#quantidade');
         this.inputValor = <HTMLInputElement> document.querySelector('#valor');
-        this.negociacoesView.update();
+        this.negociacoesView.update(this.negociacoes);
     }
 
     adiciona(event: Event) {
@@ -20,8 +20,9 @@ class NegociacaoController {
             new Date(this.inputData.value.replace(/-/g, ',')),
             parseInt(this.inputQuantidade.value),
             parseFloat(this.inputValor.value)
-        )
+            )
 
         this.negociacoes.adiciona(negociacao);
+        this.negociacoesView.update(this.negociacoes);
     }
 }
